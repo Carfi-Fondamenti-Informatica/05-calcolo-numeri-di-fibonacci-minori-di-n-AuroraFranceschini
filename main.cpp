1a969c3a7df462d84eef82f11a298d054dc976af
AuroraFranceschini <115407804+AuroraFranceschini@users.noreply.github.com>
2022-11-26 16:25:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
+    int n = 0, a = 0, b = 0, c = 0;
+    a = 1;
+    b = 1;
+    cin >> n;
+    cout << a << endl;
+    cout << b << endl;
+    for (int i = 3; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+        cout << c << endl;
+    }
    return 0;
 }
